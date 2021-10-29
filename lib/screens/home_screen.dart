f1bce8ebcbe2d0d7b148fbdfce20c04b45d0ccb4
Gabriel De La Hoz <ga.de.di@hotmail.com>
2021-10-29 13:11:51 -0500

    Commit 2

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,6 +10,27 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () => print('hi on menu icon'),
+            );
+          },
+        ), 
+        title: Text('Tienda'),
+        actions: <Widget>[
+          IconButton(
+            icon: new Icon(Icons.shopping_basket_outlined),
+            onPressed: () => print('hi on icon action'),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(),
+      )
+    );
   }
 }
